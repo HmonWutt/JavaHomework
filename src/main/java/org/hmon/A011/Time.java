87230a2a9047a577ee3ba6470496cde8e7cf8f3e
hmon <wtthumon@gmail.com>
2025-03-15 11:39:38 +0100

    added day, month, year methods

--- src/main/java/org/hmon/A011/Time.java
+++ src/main/java/org/hmon/A011/Time.java
@@ -4,12 +4,23 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Time {
+
     public static void print() {
         LocalDateTime now = LocalDateTime.now();
         System.out.println("Unformatted time: "+now);
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
         String formattedTime = now.format(formatter) ;
         System.out.println("Formatted time: "+formattedTime);
+    }
+    public static int getDay(){
+        return LocalDateTime.now().getDayOfMonth();
+
+    }
+    public static int getMonth(){
+        return LocalDateTime.now().getMonthValue();
+    }
+    public static int getYear(){
+        return LocalDateTime.now().getYear();
     }
 
 }
